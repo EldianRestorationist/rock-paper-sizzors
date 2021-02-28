8468d21f45ecb0f40c7b0ed5a2a9aee863679d13
Eldian Restorationist <eldianrestorationist@gmail.com>
2021-02-28 11:04:31 -0600

    IT FUNCTIONSgit status just need to style now

--- RPS.js
+++ RPS.js
@@ -8,8 +8,6 @@ const tallyComputer = document.querySelector("#tallyComputer");
     //variables keep track of wins
     let playerWin = 0;
     let computerWin = 0;
-    let tieGame;
-    let winner;
     //variable for computer selection
     let computerSelection;
     //function to give random computer selection
@@ -36,49 +34,46 @@ const tallyComputer = document.querySelector("#tallyComputer");
     //function to play game
     function playRound(playerSelection, computerSelection) {
         computerSelection = computerPlay();
-        if (playerSelection === "ROCK") {
-            if (computerSelection === "ROCK") {
-                tieGame += 1;
-                gameNarrative.textContent += "\nIt's a Tie";
-            } else if (computerSelection === "PAPER") {
-                computerWin += 1;
-                gameNarrative.textContent += "\nRound Lost! Paper beats Rock";
+            if (playerSelection === "ROCK") {
+                if (computerSelection === "ROCK") {
+                    gameNarrative.textContent += "\nIt's a Tie";
+                } else if (computerSelection === "PAPER") {
+                    computerWin += 1;
+                    gameNarrative.textContent += "\nRound Lost! Paper beats Rock";
+                } else {
+                    playerWin += 1;
+                    gameNarrative.textContent += "\nRound Won! Rock beats Scissors";
+                }
+            } else if (playerSelection === "PAPER") {
+                if (computerSelection === "ROCK") {
+                    playerWin += 1;
+                    gameNarrative.textContent += "\nRound Won! Paper beats Rock";
+                } else if (computerSelection === "PAPER") {
+                    gameNarrative.textContent += "\nIt's a Tie";
+                } else {
+                    computerWin += 1;
+                    gameNarrative.textContent += "\nRound Lost! Scissors beats Paper";
+                }
+            } else if (playerSelection === "SCISSORS") {
+                if (computerSelection === "ROCK") {
+                    computerWin += 1;
+                    gameNarrative.textContent += "\nRound Lost! Rock beats Scissors";
+                } else if (computerSelection === "PAPER") {
+                    playerWin += 1;
+                    gameNarrative.textContent += "\nRound Won! Scissors beats Paper";
+                } else {
+                    gameNarrative.textContent += "\nIt's a Tie";
+                }
             } else {
-                playerWin += 1;
-                gameNarrative.textContent += "\nRound Won! Rock beats Scissors";
+                gameNarrative.textContent += "Error Try Again";
             }
-        } else if (playerSelection === "PAPER") {
-            if (computerSelection === "ROCK") {
-                playerWin += 1;
-                gameNarrative.textContent += "\nRound Won! Paper beats Rock";
-            } else if (computerSelection === "PAPER") {
-                tieGame += 1;
-                gameNarrative.textContent += "\nIt's a Tie";
-            } else {
-                computerWin += 1;
-                gameNarrative.textContent += "\nRound Lost! Scissors beats Paper";
-            }
-        } else if (playerSelection === "SCISSORS") {
-            if (computerSelection === "ROCK") {
-                computerWin += 1;
-                gameNarrative.textContent += "\nRound Lost! Rock beats Scissors";
-            } else if (computerSelection === "PAPER") {
-                playerWin += 1;
-                gameNarrative.textContent += "\nRound Won! Scissors beats Paper";
-            } else {
-                tieGame += 1;
-                gameNarrative.textContent += "\nIt's a Tie";
-            }
-        } else {
-            gameNarrative.textContent += "Error Try Again";
-        }
+        //add tallies
         tallyPlayer.textContent = playerWin;
         tallyComputer.textContent = computerWin;
-        showWinner;
+        //output winner
+        showWinner();
     }
-    //function to run the game 
-        //playRound();
-    //output result
+   
     //function to restart game
     function playAgain() {
      //empty text box
@@ -86,14 +81,33 @@ const tallyComputer = document.querySelector("#tallyComputer");
      //zero out counters
      playerWin = 0;
      computerWin = 0;
+     tallyPlayer.textContent = playerWin;
+     tallyComputer.textContent = computerWin;
+     //turn buttons back on
+     enableButtons();
     }
-    againButton.onclick = playAgain();
+    //assign playAgain
+    againButton.onclick = playAgain;
+   
     //output winner
     function showWinner() {
         if (playerWin >= 5) {
-            gameNarrative.textContent = "You Win!";
+            gameNarrative.textContent += "You Win!";
+            disableButtons();
         } 
         if (computerWin >= 5) {
-            gameNarrative.textContent = "You Lose!";
+            gameNarrative.textContent += "You Lose!";
+            disableButtons();
         }
+    }
+    //turn buttons on/off
+    function disableButtons() {
+        ROCK.disabled = true;
+        PAPER.disabled = true;
+        SCISSORS.disabled = true;
+    }
+    function enableButtons() {
+        ROCK.disabled = false;
+        PAPER.disabled = false;
+        SCISSORS.disabled = false;
     }
